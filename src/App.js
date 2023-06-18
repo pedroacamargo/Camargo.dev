a8df66d42492b9180d346cd2b4354b1e55d856a3
pedroacamargo <pedroaugennes@gmail.com>
2023-06-18 02:34:44 +0100

    Nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar.component';
+import Projects from './routes/Projects.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <Navbar />
+        <Routes>
+          <Route path='/' element></Route>
+          <Route path='/projects' element={<Projects />}></Route>
+          <Route path='/about' element></Route>
+          <Route path='/contact' element></Route>
+          <Route path='*' element></Route>
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
---
+++ src/components/navbar/Navbar.component.jsx
@@ -0,0 +1,106 @@
+import { useState } from 'react';
+import './Navbar.styles.scss'
+import { Link, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpLong } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+    const [isOpened, setIsOpened] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(true);
+    const [isHover, setIsHover] = useState(false);
+    const navigate = useNavigate();
+
+    // styles
+    const ballsStyle = {
+        backgroundColor: isDarkMode ? 'rgba(235, 247, 255, 0.664)' : 'rgba(0, 0, 0, 0.8)'
+    }
+
+    const bgSwipeUp = () => {
+        if (isDarkMode && isHover) return 'black'
+        else if (isDarkMode && !isHover) return 'rgb(0, 255, 0)'
+        else if (!isDarkMode && isHover) return 'black'
+        return 'black'
+    }
+    
+    const swipeUpStyle = {
+        top: isOpened ? 'calc(100vh - 100px)' : '70px',
+        color: {bgSwipeUp},
+    }
+    
+
+    // Functions
+    const handleNavbarHeight = () => {
+        setIsOpened(!isOpened);
+    }
+
+    const changeMode = () => {
+        setIsDarkMode(!isDarkMode);
+    }
+
+    const handleMouseEnter = () => {
+        setIsHover(true);
+    };
+    
+    const handleMouseLeave = () => {
+        setIsHover(false);
+    };
+
+    const setNavbarBackground = () => {
+        if (isDarkMode && isOpened) {
+            return 'black';
+        } else if (isDarkMode && !isOpened) {
+            return 'rgb(0,0,0,0.6)'
+        } else if (!isDarkMode && isOpened) {
+            return 'black'
+        }
+        return 'rgba(0,0,0,0.8)'
+    }
+
+    return (
+        <nav style={
+                {
+                    height: isOpened ? '100vh' : '70px', 
+                    backgroundColor: isDarkMode ? 'black' : '#d8d5d5',
+                }
+            }>
+
+            <div style={ballsStyle} className='navbar-decoration dec1'></div>
+            <div style={ballsStyle} className='navbar-decoration dec2'></div>
+            <div style={ballsStyle} className='navbar-decoration dec3'></div>
+            <div style={ballsStyle} className='navbar-decoration dec4'></div>
+            <div style={ballsStyle} className='navbar-decoration dec5'></div>
+            <div style={ballsStyle} className='navbar-decoration dec6'></div>
+            <div style={ballsStyle} className='navbar-decoration dec7'></div>
+            <div style={ballsStyle} className='navbar-decoration dec8'></div>
+
+
+            <div className="navbar-container" style={
+                    {
+                        backgroundColor: `${setNavbarBackground()}`,
+                    }
+                }>
+                    
+                <div className='navbar-links'>
+                    <Link className='navbar-link' to='/' onClick={handleNavbarHeight}> Home </Link>
+                    <Link className='navbar-link' to='/projects' onClick={() => {
+                        setIsOpened(false);
+                    }}> Projects </Link>
+                    <Link className='navbar-link' to='/about'> About </Link>
+                    <Link className='navbar-link' to='/contact'> Contact </Link>
+                </div>
+
+                <button onClick={changeMode}>A</button>
+            </div>
+            
+            <button className='swipe-up' style={swipeUpStyle} onClick={() => {
+                handleNavbarHeight();
+                navigate('/projects');
+            }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}> 
+                <FontAwesomeIcon icon={faArrowUpLong} />
+                <span>Projects</span>
+            </button>
+        </nav>
+    );
+}
+
+export default Navbar;
