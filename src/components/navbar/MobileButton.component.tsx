6bc31ce2a31f343ef9d0e5c5b98f7a7a4fe4a436
pedroacamargo <pedroaugennes@gmail.com>
2023-06-27 13:58:54 +0100

    feature finish

--- src/components/navbar/MobileButton.component.tsx
+++ src/components/navbar/MobileButton.component.tsx
@@ -2,8 +2,6 @@ import '../../Styles/navbar/MobileButton.styles.scss';
 
 export const MobileButton = (props: any) => {
 
-
-    
     
     const stripState1 = {
         transform: props.isMobileMenuOpened ? 'rotate(45deg)' : '',
