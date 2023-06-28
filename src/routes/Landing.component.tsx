7a654b1f481a42fce3004ec42b9374b0459daf40
pedroacamargo <pedroaugennes@gmail.com>
2023-06-28 17:54:51 +0100

    landing background no responsive

--- src/routes/Landing.component.tsx
+++ src/routes/Landing.component.tsx
@@ -1,12 +1,14 @@
 import '../Styles/landing/landing.styles.scss'
 import WhoAmI from '../components/landing/WhoAmI.component';
 import ImageDecoration from '../components/landing/ImageDecoration.component';
+import Balls from '../components/landing/Balls.component';
 
 
 const Landing = () => {
     return (
         <div className='landing-container'>
             <div className='profile-container'>
+                <Balls />
                 <WhoAmI />
                 <ImageDecoration isOpened={true}/>
             </div>        
