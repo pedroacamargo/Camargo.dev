30b894d78a1bf9bd4800516d0447bdf1ee9dd642
MUTANTE <pedroaugennes@gmail.com>
2026-02-10 19:51:43 +0000

    Improvements about me

--- src/routes/About.component.tsx
+++ src/routes/About.component.tsx
@@ -13,16 +13,37 @@ import {
   Workflow,
   LucideBlocks,
   Baby,
+  Hammer,
+  ArrowRight,
+  ExternalLink,
 } from "lucide-react";
 
 const AboutMe = () => {
-  const experiences = [
+  const experiences: {
+    year: string;
+    role: string;
+    place: string;
+    desc: string;
+    icon: JSX.Element;
+    links: { url: string; text?: string; icon?: JSX.Element }[];
+  }[] = [
     {
       year: "March 2024 - Present",
       role: "Software Engineer",
       place: "DTx - Fulltime",
       desc: "During my bachelor's degree, I joined DTx as a researcher, receiving a research grant to further my studies while contributing to the 'Aero.Next' project. After five months, I exceeded expectations and was hired full-time. In this role, I develop web and desktop applications, manage databases, and integrate digital transformation initiatives within the aeronautical sector.",
       icon: <Plane size={22} />,
+      links: [
+        {
+          url: "https://www.linkedin.com/company/dtx-digital-transformation-colab/posts/?feedView=all",
+          icon: <Linkedin size={18} style={{ verticalAlign: "middle" }} />,
+        },
+        {
+          url: "https://www.dtx-colab.pt/",
+          icon: <ExternalLink size={18} style={{ verticalAlign: "middle" }} />,
+          text: "Website",
+        },
+      ],
     },
     {
       year: "January 2025 - Present",
@@ -30,6 +51,18 @@ const AboutMe = () => {
       place: "Play Solana - Freelancing",
       desc: "While working full-time as a fullstack developer, I joined Play Solana, a global project building a blockchain-integrated game console with a community of over 100,000 members. I developed the entire backend infrastructure for the company’s flagship game, implementing secure API protocols, data integrity checks, and access controls. I integrated Solana blockchain functionality directly into the gameplay using the Solana Unity SDK and C#, ensuring secure transactions and tamper-resistant systems. Additionally, I lead the development and maintenance of the Play Solana SDK, enabling third-party developers to build games safely on the console. This work required close collaboration with low-level systems, firmware, and C, giving me valuable expertise in virtualization for firmware simulation and secure system integration, emphasizing resiliency and protection of critical infrastructure.",
       icon: <LucideBlocks size={22} />,
+      links: [
+        {
+          url: "https://playsolana.com/",
+          text: "Website",
+          icon: <ExternalLink size={18} style={{ verticalAlign: "middle" }} />,
+        },
+        {
+          url: "https://github.com/playsolana/PlaySolana.Unity-SDK",
+          text: "PSDK",
+          icon: <Github size={18} style={{ verticalAlign: "middle" }} />,
+        },
+      ],
     },
     {
       year: "September 2023 - September 2024",
@@ -37,6 +70,17 @@ const AboutMe = () => {
       place: "Computer Science Student Association - University of Minho",
       desc: "Led the development department, developing projects and managing a team of developers to enhance the association and university events.",
       icon: <GraduationCap size={22} />,
+      links: [
+        {
+          url: "https://necc.di.uminho.pt/",
+          icon: <ExternalLink size={18} style={{ verticalAlign: "middle" }} />,
+          text: "Website",
+        },
+        {
+          url: "https://github.com/NECC",
+          icon: <Github size={18} style={{ verticalAlign: "middle" }} />,
+        },
+      ],
     },
     {
       year: "March 2023 - August 2023",
@@ -44,6 +88,7 @@ const AboutMe = () => {
       place: "NFTickit - Internship",
       desc: "Worked with OpenSeaAPI, developed new features for the company using TypeScript, GraphQL and NestJS. Developed the connection between OpenSeaAPI and the company, aswell as listing all NFTs for sale and letting users to create new offers in OpenSea website.",
       icon: <GraduationCap size={22} />,
+      links: [],
     },
     {
       year: "25/03/2004",
@@ -51,6 +96,7 @@ const AboutMe = () => {
       place: "Rio de Janeiro, Brazil",
       desc: "Born in Rio de Janeiro, Brazil, I moved to Portugal at the age of 13. From a young age, I was fascinated by technology and computers, which sparked my passion for programming and software development. This early interest laid the foundation for my future career in tech.",
       icon: <Baby size={22} />,
+      links: [],
     },
   ];
 
@@ -67,7 +113,7 @@ const AboutMe = () => {
               fontFamily: "'Share Tech Mono', monospace",
             }}
           >
-            My Experience
+            Work Experience
           </h1>
 
           <div className="timeline">
@@ -79,15 +125,42 @@ const AboutMe = () => {
                 <span className="company">{exp.place}</span>
                 <p
                   style={{
-                    color: "#ffffff",
-                    marginTop: "0.5rem",
+                    color: "#dddddd",
+                    marginBottom: "1rem",
                     fontSize: "1.1rem",
-                    lineHeight: "1.5",
                     fontFamily: "'Share Tech Mono', monospace",
                   }}
                 >
                   {exp.desc}
                 </p>
+
+                {/* Botão de Link Adicionado Aqui */}
+                {exp.links &&
+                  exp.links.map((link, linkIndex) => (
+                    <a
+                      key={linkIndex}
+                      href={link.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="experience-link"
+                    >
+                      {link?.icon ? (
+                        <span
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "5px",
+                            fontFamily: "'Share Tech Mono', monospace",
+                          }}
+                        >
+                          {link.icon}
+                          {link.text}
+                        </span>
+                      ) : (
+                        link.text
+                      )}
+                    </a>
+                  ))}
               </div>
             ))}
           </div>
