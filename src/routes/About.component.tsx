8242ecd5532bdf3494b3c701304e8fb2e43f5b3a
MUTANTE <pedroaugennes@gmail.com>
2026-02-10 19:25:12 +0000

    feat: About me in steroids

--- src/routes/About.component.tsx
+++ src/routes/About.component.tsx
@@ -1,166 +1,183 @@
 import React from "react";
 import "../Styles/about/About.styles.scss";
-import { TransitionAbout } from "../components/about/TransitionAbout.component";
-import { SectionDecorations } from "../components/about/SectionDecorations.component";
+import {
+  Briefcase,
+  GraduationCap,
+  Settings,
+  Github,
+  Linkedin,
+  Mail,
+  Plane,
+  Blocks,
+  Network,
+  Workflow,
+  LucideBlocks,
+  Baby,
+} from "lucide-react";
 
-const Pin = (props: { left: boolean; title: string, role: string }) => {
+const AboutMe = () => {
+  const experiences = [
+    {
+      year: "March 2024 - Present",
+      role: "Software Engineer",
+      place: "DTx - Fulltime",
+      desc: "During my bachelor's degree, I joined DTx as a researcher, receiving a research grant to further my studies while contributing to the 'Aero.Next' project. After five months, I exceeded expectations and was hired full-time. In this role, I develop web and desktop applications, manage databases, and integrate digital transformation initiatives within the aeronautical sector.",
+      icon: <Plane size={22} />,
+    },
+    {
+      year: "January 2025 - Present",
+      role: "Blockchain & Backend Game Developer",
+      place: "Play Solana - Freelancing",
+      desc: "While working full-time as a fullstack developer, I joined Play Solana, a global project building a blockchain-integrated game console with a community of over 100,000 members. I developed the entire backend infrastructure for the company’s flagship game, implementing secure API protocols, data integrity checks, and access controls. I integrated Solana blockchain functionality directly into the gameplay using the Solana Unity SDK and C#, ensuring secure transactions and tamper-resistant systems. Additionally, I lead the development and maintenance of the Play Solana SDK, enabling third-party developers to build games safely on the console. This work required close collaboration with low-level systems, firmware, and C, giving me valuable expertise in virtualization for firmware simulation and secure system integration, emphasizing resiliency and protection of critical infrastructure.",
+      icon: <LucideBlocks size={22} />,
+    },
+    {
+      year: "September 2023 - September 2024",
+      role: "Development Department Director",
+      place: "Computer Science Student Association - University of Minho",
+      desc: "Led the development department, developing projects and managing a team of developers to enhance the association and university events.",
+      icon: <GraduationCap size={22} />,
+    },
+    {
+      year: "March 2023 - August 2023",
+      role: "Backend Developer",
+      place: "NFTickit - Internship",
+      desc: "Worked with OpenSeaAPI, developed new features for the company using TypeScript, GraphQL and NestJS. Developed the connection between OpenSeaAPI and the company, aswell as listing all NFTs for sale and letting users to create new offers in OpenSea website.",
+      icon: <GraduationCap size={22} />,
+    },
+    {
+      year: "25/03/2004",
+      role: "The dream begins",
+      place: "Rio de Janeiro, Brazil",
+      desc: "Born in Rio de Janeiro, Brazil, I moved to Portugal at the age of 13. From a young age, I was fascinated by technology and computers, which sparked my passion for programming and software development. This early interest laid the foundation for my future career in tech.",
+      icon: <Baby size={22} />,
+    },
+  ];
+
   return (
-    <div
-      style={{
-        width: "25px",
-        height: "25px",
-        borderRadius: "50%",
-        backgroundColor: "yellow",
-        margin: "10px 10px",
-      }}
-    >
-      <div
-        style={{
-          position: "relative",
-          top: "50%",
-          color: "white",
-        }}
-      >
-        <span
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: props.left ? "0" : "100%",
-            transform: props.left
-              ? "translate(-103%, -60%)"
-              : "translate(3%, -50%)",
-            color: "white",
-            fontFamily: "monospace",
-            fontSize: "16px",
-            whiteSpace: "nowrap",
-          }}
-        >
-          {props.role}
-        </span>
-        <span
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: props.left ? "100%" : "0",
-            transform: props.left
-              ? "translate(5%, -50%)"
-              : "translate(-105%, -60%)",
-            color: "red",
-            fontFamily: "monospace",
-            fontSize: "12px",
-            whiteSpace: "nowrap",
-          }}
-        >
-          {props.title}
-        </span>
+    <div className="about-container">
+      <div className="content-wrapper">
+        {/* Lado Esquerdo: Timeline */}
+        <section>
+          <h1
+            style={{
+              color: "#ff4d4d",
+              fontSize: "3rem",
+              marginBottom: "1rem",
+              fontFamily: "'Share Tech Mono', monospace",
+            }}
+          >
+            My Experience
+          </h1>
+
+          <div className="timeline">
+            {experiences.map((exp, index) => (
+              <div key={index} className="timeline-item">
+                <div className="icon-wrapper">{exp.icon}</div>
+                <span className="year">{exp.year}</span>
+                <h3>{exp.role}</h3>
+                <span className="company">{exp.place}</span>
+                <p
+                  style={{
+                    color: "#ffffff",
+                    marginTop: "0.5rem",
+                    fontSize: "1.1rem",
+                    lineHeight: "1.5",
+                    fontFamily: "'Share Tech Mono', monospace",
+                  }}
+                >
+                  {exp.desc}
+                </p>
+              </div>
+            ))}
+          </div>
+          <div
+            style={{
+              position: "relative",
+              left: "17px",
+              top: "-50px",
+              width: "10px",
+              height: "10px",
+              backgroundColor: "#330707",
+              borderRadius: "2px",
+              marginLeft: "-2px",
+            }}
+          ></div>
+        </section>
+
+        <aside>
+          <h2
+            style={{
+              color: "#ff4d4d",
+              marginBottom: "1.5rem",
+              fontFamily: "'Share Tech Mono', monospace",
+            }}
+          >
+            My Skills & Background
+          </h2>
+          <p
+            style={{
+              color: "#dddddd",
+              marginBottom: "3rem",
+              fontSize: "1.2rem",
+              lineHeight: "1.6",
+              fontFamily: "'Share Tech Mono', monospace",
+            }}
+          >
+            Native Portuguese speaker with advanced English proficiency. I have
+            a proven track record in developing fullstack web applications using
+            technologies such as Next.js, TypeScript, Express.js, Prisma,
+            PostgreSQL, and the Solana blockchain, with a strong focus on
+            implementing secure coding practices, ensuring data integrity, and
+            building systems resilient against potential cyber threats. Outside
+            of tech, I apply the same discipline as a high-level athlete, I'm a
+            seven-time national champion water polo goalkeeper and have competed
+            twice in the Champions League.
+          </p>
+          <table className="skills-table">
+            <thead>
+              <tr>
+                <th>Category</th>
+                <th>Tech & Tools</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Security & Systems</td>
+                <td>Linux, Docker, VirtualBox, Kali Linux, Bash, GitHub</td>
+              </tr>
+              <tr>
+                <td>Backend & Database</td>
+                <td>
+                  Node.js, TypeScript, JavaScript, Express, C, C++, C#,
+                  PostgreSQL, MySQL, NestJS, Prisma, Python, GraphQL
+                </td>
+              </tr>
+              <tr>
+                <td>Blockchain</td>
+                <td>Solana</td>
+              </tr>
+              <tr>
+                <td>Currently Studying</td>
+                <td>
+                  Reverse Engineering, Malware Analysis, CompTIA Security+
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          {/* <div style={{ marginTop: '40px' }}>
+            <h2 style={{ color: '#ff4d4d', marginBottom: '1rem' }}>Contacto</h2>
+            <div style={{ display: 'flex', gap: '15px' }}>
+              <Github className="hover-red" cursor="pointer" />
+              <Linkedin className="hover-red" cursor="pointer" />
+              <Mail className="hover-red" cursor="pointer" />
+            </div>
+          </div> */}
+        </aside>
       </div>
     </div>
   );
 };
 
-const Line = () => {
-  return (
-    <div
-      style={{
-        height: "40px",
-        width: "5px",
-        backgroundColor: "yellow",
-        top: "50%",
-      }}
-    ></div>
-  );
-};
-
-const About = () => {
-  const data = [
-    {
-      id: 1,
-      name: "Pin",
-      component: <Pin left title="March 2024 - Present" role="Fullstack engineer @DTx" />,
-    },
-    {
-      id: 2,
-      name: "Line",
-      component: <Line />,
-    },
-    {
-      id: 3,
-      name: "Pin",
-      component: <Pin left={false} title="January 2025 - Present" role="Blockchain Game Developer @ Play Solana" />,
-    },
-    {
-      id: 4,
-      name: "Line",
-      component: <Line />,
-    },
-    {
-      id: 5,
-      name: "Pin",
-      component: <Pin left title="September 2023 - Present" role="Development Depart Director @NECC" />,
-    },
-    {
-      id: 6,
-      name: "Line",
-      component: <Line />,
-    },
-    {
-      id: 7,
-      name: "Pin",
-      component: <Pin left={false} title="Intership 2022/2023" role="Backend Developer @ NFTickit" />,
-    },
-    {
-      id: 8,
-      name: "Line",
-      component: <Line />,
-    },
-    {
-        id: 7,
-        name: "Pin",
-        component: <Pin left={true} title="25/03/2004 - Birth" role="The dream started" />,
-    },
-  ];
-
-  return (
-    <section className="about-container">
-      <SectionDecorations />
-
-      <article
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        {data.map((item) => (
-          <div
-            key={item.id}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            {item.component}
-          </div>
-        ))}
-      </article>
-
-      <TransitionAbout />
-
-
-      <div style={{
-        position: "absolute",
-        bottom: "50px",
-        zIndex: 100,
-        backgroundColor: "black",
-        color: "white",
-      }}>
-        This page is under construction (I don&apos; have time to finish it yet 😅)
-      </div>
-    </section>
-  );
-};
-
-export default About;
+export default AboutMe;
